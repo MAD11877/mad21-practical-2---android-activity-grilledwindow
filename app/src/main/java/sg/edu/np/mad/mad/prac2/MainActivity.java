5d68443138d3b28cac8db361583c8afc3eae62b4
Xavier Ang <xave@ctemplar.com>
2021-04-29 11:10:57 +0800

    First submission

---
+++ app/src/main/java/sg/edu/np/mad/mad/prac2/MainActivity.java
@@ -0,0 +1,50 @@
+package sg.edu.np.mad.mad.prac2;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private final static String TAG = "Main Activity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Log.v(TAG, "Main Activity Created");
+
+//        SharedPreferences sharedPref = getActivity().getPreferences(Context.MODE_PRIVATE);
+//        boolean followed =
+        User user = new User(false);
+
+        TextView title = findViewById(R.id.title);
+        title.setText("Hello World!");
+
+        TextView lorem = findViewById(R.id.lorem);
+        lorem.setText("Lorem ipsum dolor, sit amet consectetur adipisicing elit. Unde facere aliquid quaerat enim excepturi, hic suscipit quam blanditiis eum laborum vero, iure, mollitia recusandae architecto quod porro aperiam rem eveniet.");
+
+        Button followBtn = findViewById(R.id.follow_btn);
+        followBtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view)
+            {
+                user.setFollowed(!user.isFollowed());
+                followBtn.setText(user.isFollowed()
+                        ? "UNFOLLOW"
+                        : "FOLLOW"
+                );
+            }
+        });
+    }
+}
